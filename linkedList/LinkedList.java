bec388a6e25d282ef4b0f3735165ae4adb599d61
JChase2 <mr.jdchase@gmail.com>
2018-02-21 16:50:18 -0500

    Initial Commit

---
+++ linkedList/LinkedList.java
@@ -0,0 +1,20 @@
+/* Author: James Chase
+*  Just practicing data structures in java. 
+*  Linked List...
+*/
+
+
+/**
+ *
+ * @author john
+ */
+public class LinkedList {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}

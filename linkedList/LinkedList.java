ff064603d3a44e1b44dd27aafb07e16a0aeb4b50
JChase2 <mr.jdchase@gmail.com>
2018-02-23 11:48:42 -0500

    Basic linked list, minimal features.

--- linkedList/LinkedList.java
+++ linkedList/LinkedList.java
@@ -1,20 +1,66 @@
-/* Author: James Chase
+/* Author: JChase2
 *  Just practicing data structures in java. 
 *  Linked List...
-*/
-
-
-/**
- *
- * @author john
+*  Heavily commented for reference  and learning purposes...
  */
-public class LinkedList {
+
+public class LinkedList<T>
+{
+
+    private Node<T> head; // Current object at head. 
+    private Node<T> next;
+
+    // Empty constructor for LinkedList
+    public LinkedList()
+    {
+        head = null;
+        next = null;
+    }
+
+    // Add node to front of list. 
+    // E.g. create a new head. 
+    public void addNodeFront(T item)
+    {
+        Node newNode = new Node(item);
+        newNode.next = head;
+        head = newNode;
+    }
+
+    // Node class. 
+    private class Node<T>
+    {
+
+        private T data; // Data in current node object. 
+        private Node<T> next; // Points to next object after current node. 
+
+        // Constructor. 
+        public Node(T item)
+        {
+            this.data = item;
+            this.next = null;
+        }
+
+        // Get data in current node. 
+        public T getData()
+        {
+            return this.data;
+        }
+
+        public void setData(T item)
+        {
+            this.data = item;
+        }
+
+    }
 
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
-        // TODO code application logic here
+    public static void main(String[] args)
+    {
+        LinkedList newList = new LinkedList();
+        newList.addNodeFront("test");
+        System.out.println(newList.head.getData());
+        System.out.println(newList.next);
     }
-    
 }

b520ebf0180487676874c87a2ad47132ed0bed96
JChase2 <mr.jdchase@gmail.com>
2018-02-27 15:54:02 -0500

    Rewrote most stuff, it's a doubly linked list now. Still pretty simple. Not circular, but that's fine. This is pretty much all I had in mind when writing this.

--- linkedList/LinkedList.java
+++ linkedList/LinkedList.java
@@ -1,6 +1,6 @@
 /* Author: JChase2
 *  Just practicing data structures in java. 
-*  Generic Linked List. 
+*  Generic Doubly Linked List. 
 *  Heavily commented for reference  and learning purposes...
  */
 
@@ -8,11 +8,46 @@ public class LinkedList<T>
 {
 
     private Node<T> head; // Current object at head. 
+    private Node<T> tail; // Current object at tail. 
 
     // Empty constructor for LinkedList
     public LinkedList()
     {
         head = null;
+    }
+    
+    public void insertBefore(T item, Node beforeNode)
+    {
+        Node newNode = new Node(item);
+        newNode.next = beforeNode;
+        if (beforeNode.previous == null)
+        {
+            newNode.previous = null;
+            head = newNode;
+        }
+        else 
+        {
+            newNode.previous = beforeNode.previous;
+            beforeNode.previous.next = newNode;
+            beforeNode.previous = newNode;
+        }
+    }
+    
+    public void insertAfter(T item, Node afterNode)
+    {
+        Node newNode = new Node(item);
+        newNode.previous = afterNode;
+        if (afterNode.next == null)
+        {
+            newNode.next = null;
+            tail = newNode;
+        }
+        else 
+        {
+            newNode.next = afterNode.next;
+            afterNode.next.previous = newNode;
+            afterNode.next = newNode;
+        }
     }
 
     // Add node to front of list. 
@@ -20,13 +55,41 @@ public class LinkedList<T>
     public void addHead(T item)
     {
         Node newNode = new Node(item);
-        newNode.next = head;
-        head = newNode;
+        if (head == null)
+        {
+            head = newNode;
+            tail = newNode;
+            head.next = null;
+            head.previous = null;
+        }
+        else 
+            insertBefore(item, head);
     }
     
-    public void removeHead ()
+    public void removeNode(Node node)
     {
-        head = head.next;
+        if (node.previous == null)
+        {
+            head = node.next;
+        }
+        else
+            node.previous.next = node.next;
+        if (node.next == null)
+        {
+            tail = node.previous;
+        }
+        else
+            node.next.previous = node.previous;
+    }
+    
+    // Adds a Tail to the list. 
+    public void addTail(T item)
+    {
+        Node newNode = new Node(item);
+        newNode.previous = tail;
+        newNode.next = null;
+        tail.next = newNode;
+        tail = newNode; 
     }
     
     public void printAll ()
@@ -48,12 +111,14 @@ public class LinkedList<T>
 
         private T data; // Data in current node object. 
         private Node<T> next; // Points to next object after current node. 
+        private Node<T> previous; // Points to previous Node. 
 
         // Constructor. 
         public Node(T item)
         {
             this.data = item;
             this.next = null;
+            this.previous = null;
         }
 
         // Get data in current node. 
@@ -79,6 +144,11 @@ public class LinkedList<T>
         newList.addHead("oh");
         newList.addHead("kk");
         newList.addHead("finished");
+        newList.addTail("end node test");
+        newList.printAll();
+        newList.removeNode(newList.head);
+        newList.printAll();
+        newList.removeNode(newList.tail);
         newList.printAll();
         
     }

baa98ede2e86a9a05bde6370b179f102dec720ba
JChase2 <mr.jdchase@gmail.com>
2018-04-03 16:36:25 -0400

    Basic stack implementation using linked list.

--- stack/src/stack/Stack.java
+++ stack/src/stack/Stack.java
@@ -1,23 +1,38 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+/* Author: JChase2
+*  Just practicing data structures in java. 
+*  Using a jar file generated from the linked 
+* list application in this repo to build this stack. 
  */
 package stack;
 
-/**
- *
- * @author john
- */
-public class Stack
-{
+import linkedlist.*;
 
+public class Stack <T>
+{
+    Linkedlist.Node head; // Current object at head. 
+    
+    public void push(Linkedlist aList, T item)
+    {
+        Linkedlist.Node aNode = new Linkedlist.Node(item);
+        aList.addHead(item);
+        head = aNode;
+    }
+    public String pop (Linkedlist aList)
+    {
+        String aReturn = head.getData().toString();
+        aList.removeNode(head);
+        return aReturn;
+    }
+    
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args)
     {
-        // TODO code application logic here
+        Linkedlist aList = new Linkedlist();
+        Stack newStack = new Stack();
+        newStack.push(aList, 1);
+        System.out.println(newStack.pop(aList));
     }
-    
+
 }
